7d48e8d2e31276d672924603c3ff4ea04c5691d0
mamartin <mamartin@oesia.com>
2019-06-11 13:45:29 +0200

    con rest-assured

--- model/src/main/java/com/altran/mamartin/model/services/BaseService.java
+++ model/src/main/java/com/altran/mamartin/model/services/BaseService.java
@@ -1,23 +1,40 @@
 package com.altran.mamartin.model.services;
 
+import static org.springframework.security.config.Elements.HTTP;
+
+import com.altran.mamartin.beans.utils.Constants;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public abstract class BaseService<T> {
 
   @Autowired
   protected ApplicationContext applicationContext;
+  @Value("${app.server.hostname}")
+  private String hostname;
+
+  @Value("${server.port}")
+  private String port;
+
+  @Value("${server.servlet.context-path}")
+  private String path;
 
   protected Page<T> getPagination(Pageable pageable, List<T> alls) {
     PagedListHolder<T> pagedListHolder = new PagedListHolder<>(alls);
     pagedListHolder.setPageSize(pageable.getPageSize());
     pagedListHolder.setPage(pageable.getPageNumber());
     return new PageImpl<>(pagedListHolder.getPageList(), pageable, alls.size());
+  }
+
+  protected UriComponentsBuilder getUriBuilder(String pathResource) {
+    return UriComponentsBuilder.newInstance().scheme(HTTP).host(hostname).port(port).path(path).path(pathResource).path(Constants.SLASH);
   }
 
 }
---
+++ webcontroller/src/test/java/com/altran/mamartin/webcontroller/EntityResourceAssured.java
@@ -0,0 +1,73 @@
+package com.altran.mamartin.webcontroller;
+
+import static io.restassured.RestAssured.given;
+
+import com.altran.mamartin.beans.dto.Entity;
+import com.altran.mamartin.beans.utils.Constants;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ResponseBody;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+
+public class EntityResourceAssured {
+
+  private static final String URL = "http://localhost:8080/rest/api/v1/entity";
+
+  @Test
+  public void findAll() {
+    List<Entity> result = given()
+        .contentType(ContentType.JSON)
+        .get(URL + "/all")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .extract()
+        .response()
+        .body()
+        .as(List.class);
+    Assert.assertNotNull(result);
+    Assert.assertTrue(result.size() > 0);
+  }
+
+  @Test
+  public void findAllPaginated() {
+    ResponseBody body = given()
+        .contentType(ContentType.JSON)
+        .get(URL + "/?page=2&size=3")
+        .then() mvn test -Prun - test - e2e
+        .statusCode(HttpStatus.OK.value())
+        .extract()
+        .response()
+        .body();
+    JsonPath jsonPath = body.jsonPath();
+    List<Entity> result = jsonPath.getList("content");
+    Assert.assertNotNull(result);
+    Assert.assertTrue(3 == result.size());
+    Map<String, Object> pageable = jsonPath.get("pageable");
+    Assert.assertNotNull(pageable);
+    Assert.assertTrue(3 == (Integer) pageable.get("pageSize"));
+    Assert.assertTrue(2 == (Integer) pageable.get("pageNumber"));
+  }
+
+  @Test
+  public void findById() {
+    final String id = "050453ef-14a3-4841-b0b4-368de0aeda0e";
+    String uri = URL + Constants.SLASH + id;
+    Entity result = given()
+        .contentType(ContentType.JSON)
+        .get(uri)
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .extract()
+        .response()
+        .body()
+        .as(Entity.class);
+    Assert.assertNotNull(result);
+    Assert.assertTrue(id.equals(result.getId()));
+    Assert.assertEquals(uri, result.getUri());
+  }
+
+}
